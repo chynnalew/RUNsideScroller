59901ea05da04f9b18738821400ab0ee64b146ff
chynnalew <chynnalew@yahoo.com>
2021-10-01 01:37:04 -0700

    update webpack

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,8 +6,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "",
-    // publicPath: "/repo-name-here/"
+    filename: "bundle.min.js"
   },
   devtool: false,
   performance: {
